514522361cf639f0830dde5107638ef1654c4563
CHRIS <cbjornst@nu.edu>
2019-10-05 18:21:00 -0400

    Refactor code

--- build_tagger.py
+++ build_tagger.py
@@ -1,28 +1,26 @@
 # python3.5 build_tagger.py <train_file_absolute_path> <model_file_absolute_path>
 
-import os
-import math
 import sys
 import time
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
 import torch.nn.utils.rnn as rnn
 from torch.autograd import Variable
-import numpy as np
 import random
     
 class Model(nn.Module):
     def __init__(self, V):
         #initialize the model
         super(Model, self).__init__()
-        self.lstm = nn.LSTM(15, 15, bidirectional=True, batch_first=True)
-        self.wordEmbeds = nn.Embedding(V, 15)
         self.batch_size = 64
-        self.charEmbeds = nn.Embedding(85, 15)
-        self.conv = nn.Conv1d(1, 15, 45)
-        self.linear = nn.Linear(30, 45)
+        self.hSize = 20
+        self.numTags = 45
+        self.lstm = nn.LSTM(self.hSize, self.hSize, bidirectional=True, batch_first=True)
+        self.wordEmbeds = nn.Embedding(V, self.hSize)
+        self.charEmbeds = nn.Embedding(85, self.hSize)
+        self.conv = nn.Conv1d(1, self.hSize, self.numTags)
+        self.linear = nn.Linear(self.hSize * 2, self.numTags)
         self.hidden = self.init_hidden()
         
     def forward(self, data, innerSize, lengths):
@@ -36,47 +34,48 @@ class Model(nn.Module):
         unpacked = unpacked.contiguous()
         unpacked = unpacked.view(-1, unpacked.shape[2])
         result = self.linear(unpacked)
-        result = result.view(self.batch_size, innerSize, 45)
+        result = result.view(self.batch_size, innerSize, self.numTags)
         return result
     
     def init_hidden(self):
         #re-initialize hidden vector as pytorch does not automatically do so
-        return (Variable(torch.torch.randn(2, self.batch_size, 15)), Variable(torch.torch.randn(2, self.batch_size, 15))) 
+        return (Variable(torch.torch.randn(2, self.batch_size, self.hSize)), Variable(torch.torch.randn(2, self.batch_size, self.hSize))) 
+       
+    def charEmbedding(self, word):
+        wordLen = len(word)
+        wordEmbed = None
+        index = 0
+        for letter in range(1, wordLen):
+            letters = self.dictionary[word[letter].item()]
+            charEmbed = None
+            for j in range(-1, 2):    
+                if (letter + j) > 0 and (letter + j) < wordLen:
+                    c = [self.chars[word[letter + j]]]
+                    c = torch.LongTensor(c)
+                else:
+                    c = [84]
+                    c = torch.LongTensor(c)
+            c = self.cEmbeds(c)
+            if charEmbed is not None:
+                charEmbed = torch.cat((charEmbed, c), 1)
+            else:
+                charEmbed = c
+        charEmbed = charEmbed.unsqueeze(0)
+        c = self.conv(charEmbed)
+        if wordEmbed is not None:
+            wordEmbed = torch.cat((wordEmbed, c), 2)
+        else:
+            wordEmbed = c
+        maximum = nn.MaxPool1d(len(letters))
+        w = maximum(wordEmbed)
+        w = w.view([1, self.hSize])
+        #w = torch.cat((embedding[index], w), 1)
+        index += 1
         
     def wordEmbedding(self, words):
         #create word embeddings
-        #eventually will also create character embeddings; this is a work in 
-        #progress
-        e = self.wordEmbeds(words)
-        ci = None
-        for i in range(len(words)):    
-            for j in range(len(words[i])):
-                if words[i][j] != 0:
-                    word = self.dictionary[words[i][j].item()]
-                    cj = None
-                    for j in range(3):    
-                        if (i + j - 1) > 0 and (i + j - 1) < len(word):
-                            c = [self.chars[word[i + j - 1]]]
-                            c = torch.LongTensor(c)
-                        else:
-                            c = [84]
-                            c = torch.LongTensor(c)
-                        c = self.cEmbeds(c)
-                        if cj is not None:
-                            cj = torch.cat((cj, c), 1)
-                        else:
-                            cj = c
-                    cj = cj.unsqueeze(0)
-                    c = self.conv(cj)
-                    if ci is not None:
-                        ci = torch.cat((ci, c), 2)
-                    else:
-                        ci = c
-            maximum = nn.MaxPool1d(len(word))
-            w = maximum(ci)
-            w = w.view([1, 15])
-            w = torch.cat((e[i], w), 1)
-        return e
+        embedding = self.wordEmbeds(words)
+        return embedding
     
 learning_rate = 1
 
@@ -92,28 +91,27 @@ def train_model(train_file, model_file):
     data = data.read().splitlines()
     dictionary = {"<PAD>": 0}
     chars = {'I': 0, 'n': 1, 'a': 2, 'O': 3, 'c': 4, 't': 5, '.': 6, '1': 7, '9': 8, 'r': 9, 'e': 10, 'v': 11, 'i': 12, 'w': 13, 'o': 14, 'f': 15, '`': 16, 'T': 17, 'h': 18, 'M': 19, 's': 20, 'p': 21, "'": 22, 'C': 23, 'g': 24, 'G': 25, 'd': 26, 'm': 27, '(': 28, 'R': 29, 'l': 30, 'z': 31, 'k': 32, 'S': 33, 'W': 34, 'y': 35, ',': 36, 'L': 37, 'u': 38, '&': 39, 'A': 40, ')': 41, 'b': 42, 'K': 43, 'H': 44, 'E': 45, '-': 46, 'x': 47, 'U': 48, '2': 49, '0': 50, '4': 51, 'B': 52, 'F': 53, 'N': 54, 'D': 55, 'q': 56, '5': 57, '8': 58, '7': 59, '%': 60, '{': 61, '}': 62, 'j': 63, '/': 64, '$': 65, '3': 66, 'Y': 67, ':': 68, 'P': 69, 'Q': 70, 'J': 71, 'V': 72, '?': 73, ';': 74, 'Z': 75, '6': 76, '#': 77, 'X': 78, '!': 79, '\\': 80, '*': 81, '=': 82, '@': 83}
-    tags = {"<PAD>": 45, "$": 0,"#": 1,"``": 2,"''": 3,"-LRB-": 4,"-RRB-": 5,",": 6,".": 7,":": 8,"CC": 9,"CD": 10,"DT": 11,"EX": 12,"FW": 13,"IN": 14,"JJ": 15,"JJR": 16,"JJS": 17,"LS": 18,"MD": 19,"NN": 20,"NNP": 21,"NNPS": 22,"NNS": 23,"PDT": 24,"POS": 25,"PRP": 26,"PRP$": 27,"RB": 28,"RBR": 29,"RBS": 30,"RP": 31,"SYM": 32,"TO": 33,"UH": 34,"VB": 35,"VBD": 36,"VBG": 37,"VBN": 38,"VBP": 39,"VBZ": 40,"WDT": 41,"WP": 42,"WP$": 43,"WRB": 44}
+    tagList = {"<PAD>": 45, "$": 0,"#": 1,"``": 2,"''": 3,"-LRB-": 4,"-RRB-": 5,",": 6,".": 7,":": 8,"CC": 9,"CD": 10,"DT": 11,"EX": 12,"FW": 13,"IN": 14,"JJ": 15,"JJR": 16,"JJS": 17,"LS": 18,"MD": 19,"NN": 20,"NNP": 21,"NNPS": 22,"NNS": 23,"PDT": 24,"POS": 25,"PRP": 26,"PRP$": 27,"RB": 28,"RBR": 29,"RBS": 30,"RP": 31,"SYM": 32,"TO": 33,"UH": 34,"VB": 35,"VBD": 36,"VBG": 37,"VBN": 38,"VBP": 39,"VBZ": 40,"WDT": 41,"WP": 42,"WP$": 43,"WRB": 44}
     V = 1
     trainingData = []
     labels = []
     #create dictionary of words
     for line in data:
         line = line.split(" ")
-        t1 = []
-        t2 = []
-        for word in line:
-            word1, word2 = word.rsplit("/", 1)
-            word1 = str(word1)
-            word2 = str(word2)
-            if word1 not in dictionary:
-                dictionary[word1] = V
+        words = []
+        tags = []
+        for taggedWord in line:
+            word, tag = taggedWord.rsplit("/", 1)
+            word = str(word)
+            tag = str(tag)
+            if word not in dictionary:
+                dictionary[word] = V
                 V += 1
-            t1 += [dictionary[word1]]
-            t2 += [tags[word2]]
-        trainingData += [torch.LongTensor(t1)]
-        labels += [torch.LongTensor(t2)]
+            words += [dictionary[word]]
+            tags += [tagList[tag]]
+        trainingData += [torch.LongTensor(words)]
+        labels += [torch.LongTensor(tags)]
     tagger = Model(V + 1)
-    #tagger = tagger.cuda()
     tagger.dictionary = dictionary
     tagger.chars = chars
     #use cross entropy loss and SGD for the neural net
@@ -122,14 +120,15 @@ def train_model(train_file, model_file):
     start = time.clock()
     batches = []
     i = 0
-    while i < len(trainingData):
-        if i + 64 > len(trainingData):
+    tdSize = len(trainingData)
+    while i < tdSize:
+        if i + 64 > tdSize:
             batches += [(trainingData[i:], labels[i:])]
         else:
             batches += [(trainingData[i:i+64], labels[i:i+64])]
         i += 64
     train_loss_ = []
-    for epoch in range(5):        
+    for epoch in range(10):        
         #manually shuffle and pass batches in as the built-in pytorch function
         #was having problems
         optimizer = adjust_learning_rate(optimizer, epoch)        
--- run_tagger.py
+++ run_tagger.py
@@ -1,11 +1,7 @@
 # python3.5 run_tagger.py <test_file_absolute_path> <model_file_absolute_path> <output_file_absolute_path>
 
-import os
-import math
 import sys
 import torch
-import numpy as np
-import torch.nn.functional as F
 import torch.nn as nn
 import torch.nn.utils.rnn as rnn
 from torch.autograd import Variable
@@ -31,7 +27,7 @@ def tag_sentence(test_file, model_file, out_file):
 def forward2(self, data):
     embeddings = None
     t1 = []
-    self.hidden = (torch.zeros(2, 1, 15), torch.zeros(2, 1, 15))
+    self.hidden = (torch.zeros(2, 1, 20), torch.zeros(2, 1, 20))
     for word in data:
         if word in self.dictionary:
             t1 += [self.dictionary[word]]
@@ -39,20 +35,23 @@ def forward2(self, data):
             t1  += [len(self.dictionary) - 1]
     t1 = torch.LongTensor(t1)
     embeddings = self.wordEmbeds(t1)
-    probs, self.hidden = self.lstm(embeddings.view(len(data), 1, 15), self.hidden)
+    probs, self.hidden = self.lstm(embeddings.view(len(data), 1, 20), self.hidden)
     result = self.linear(probs)
     result = torch.argmax(result, dim=2)
     return result
 
 class Model(nn.Module):
     def __init__(self, V):
+        #initialize the model
         super(Model, self).__init__()
-        self.lstm = nn.LSTM(15, 15, bidirectional=True, batch_first=True)
-        self.wordEmbeds = nn.Embedding(V, 15)
         self.batch_size = 64
-        #self.charEmbeds = nn.Embedding(85, 15)
-        #self.conv = nn.Conv1d(1, 15, 45)
-        self.linear = nn.Linear(30, 45)
+        self.hSize = 20
+        self.numTags = 45
+        self.lstm = nn.LSTM(self.hSize, self.hSize, bidirectional=True, batch_first=True)
+        self.wordEmbeds = nn.Embedding(V, self.hSize)
+        #self.charEmbeds = nn.Embedding(85, self.hSize)
+        #self.conv = nn.Conv1d(1, self.hSize, self.numTags)
+        self.linear = nn.Linear(self.hSize * 2, self.numTags)
         self.hidden = self.init_hidden()
         
     def forward(self, data, innerSize, lengths):
@@ -64,11 +63,11 @@ class Model(nn.Module):
         unpacked = unpacked.contiguous()
         unpacked = unpacked.view(-1, unpacked.shape[2])
         result = self.linear(unpacked)
-        result = result.view(self.batch_size, innerSize, 45)
+        result = result.view(self.batch_size, innerSize, self.numTags)
         return result
     
     def init_hidden(self):
-        return (Variable(torch.torch.randn(2, self.batch_size, 15).cuda()), Variable(torch.torch.randn(2, self.batch_size, 15).cuda())) 
+        return (Variable(torch.torch.randn(2, self.batch_size, self.hSize)), Variable(torch.torch.randn(2, self.batch_size, self.hSize))) 
         
     def wordEmbedding(self, dictionary, chars, words, wEmbeds, cEmbeds, conv):
         w1 = []
